5e7299a7a450ae6ee115bd8924b143f29a1fc7da
Volney Faustini <volney.faustini@gmail.com>
2021-02-22 21:45:59 -0300

    sigcourse and messages routes done

--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -14,11 +14,15 @@ router.get("/messages/:id/", (req, res) => {
     .catch((err) => console.log(`Error while getting the messages from the DB: ${err}`));
 });
 
-
-// router.post('/sendmessage', (req, res, next) => {
-
-//     const { mensagem } = req.body;
-
+router.post("/sendmessage/:to/", (req, res) => {
+    const { to } = req.params;
+  const from = req.session.currentUser._id;
+    const message =  req.body.mensagem;
+    const status = "Enviado"
+    Message.create({message, from, to, status})
+    .then(sentMessage => res.redirect(`/user/${from}`))
+    .catch((err) => console.log(`Error while sending the messages to the DB: ${err}`));
+});
 
 module.exports = router;
 
