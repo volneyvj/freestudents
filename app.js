b1624a050271ad1f7a82590f5e9e9cc64faf342f
Volney Faustini <volney.faustini@gmail.com>
2021-03-03 11:13:45 -0300

    title

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
@@ -44,7 +44,7 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app/assets')))
+// app.use(express.static(path.join(__dirname, 'app/assets')))
 // app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
